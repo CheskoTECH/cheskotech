782272a44250348adaa3a2f66365192c370701fa
CheskoTECH <cheskidov.peter@yandex.ru>
2019-02-03 22:12:29 +0500

    some opti3

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:800,900' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:800,900|Roboto+Slab' }
     ]
   },
   /*
