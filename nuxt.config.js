af61677821b250b5a6e0447f3bd8684f83d76d04
CheskoTECH <cheskidov.peter@yandex.ru>
2019-02-04 07:38:52 +0500

    some opti5

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ module.exports = {
   ],
   router: {
     //base: '/', // for dev
-    base: '/chesko.tech/' // for deploy, need to change on chesko.tech
+    base: '/' // for deploy, need to change on chesko.tech
   },
   /*
   ** Build configuration
